7c72356f431ee6fe4a3cf9a08caeac862bbb87c7
basbuijsen <bbuijsen@gmail.com>
2022-01-04 16:39:18 +0100

    initial 2

---
+++ BRESS-TournooiPlanner/BRESS_TournooiPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRESS_TournooiPlannerApp.swift
+//  BRESS-TournooiPlanner
+//
+//  Created by Bas Buijsen on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BRESS_TournooiPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BRESS-TournooiPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BRESS-TournooiPlanner
+//
+//  Created by Bas Buijsen on 04/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
